6e2e8c1f540776a9bc51c6a4bd1661f8a5537dc0
TINA MYERS <myers.tina515@gmail.com>
2021-03-01 15:38:27 -0800

    still working on CORS issue

--- src/server.js
+++ src/server.js
@@ -20,8 +20,13 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   next();
 });
 
