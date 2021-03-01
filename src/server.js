6ccefc91481e906618858ef6cc2f2ad53529ac73
TINA MYERS <myers.tina515@gmail.com>
2021-03-01 15:33:43 -0800

    attempting to resolve CORS issue

--- src/server.js
+++ src/server.js
@@ -20,6 +20,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 // app.get('/', (req, res) => {
 //   console.log('routes connected');
