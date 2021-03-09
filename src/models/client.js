a74e934ce7da9227a733a20fe0c0d6b53a5a7d44
TINA MYERS <myers.tina515@gmail.com>
2021-03-09 12:27:44 -0800

    changing data type to strings

--- src/models/client.js
+++ src/models/client.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const clientSchema = mongoose.Schema({
   name: {type: String, required: true},
   emailAddress: {type: String, required: false},
-  phoneNumber: {type: Number, required: false},
+  phoneNumber: {type: String, required: false},
   trainingType:{type: String, required: false},
   injury: {type: String, required: false},
   goals: {type: String, required: false},
   message: {type: String, required: false},
-  contacted: {type: Boolean, require: false}
+  contacted: {type: String, require: false}
 })
 
 const clientModel = mongoose.model('client', clientSchema);
