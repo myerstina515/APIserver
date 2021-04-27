fea31632e36d601e30831b41e98c6ccf5585d2ff
TINA MYERS <myers.tina515@gmail.com>
2021-04-26 17:55:50 -0700

    adding newsletter preference and where the client was referred from

--- src/models/client.js
+++ src/models/client.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const clientSchema = mongoose.Schema({
   name: {type: String, required: true},
   emailAddress: {type: String, required: false},
+  newsletter: {type: String, required: false},
   phoneNumber: {type: String, required: false},
   trainingType:{type: String, required: false},
   injury: {type: String, required: false},
   goals: {type: String, required: false},
   message: {type: String, required: false},
-  contacted: {type: String, require: false}
+  contacted: {type: String, require: false},
+  routedFrom: {type: String, require: false}
 })
 
 const clientModel = mongoose.model('client', clientSchema);
