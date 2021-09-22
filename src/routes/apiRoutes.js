9e0457167b77de83cb929ec5749cb4659e186ff9
TINA MYERS <myers.tina515@gmail.com>
2021-09-22 16:57:18 -0700

    initializing testing folder and creating first test

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,48 +1,11 @@
 'use strict';
 
 const express = require('express');
-// const app = express();
-// const cors = require('cors');
 const router = express.Router();
 const Collection = require('../models/dataCollection');
 const client = new Collection();
-// console.log('made it to the routes page');
-// console.log('this is the collection', Collection);
-// console.log('this is the client', client);
-// app.use(cors());
 
-// app.use(function (req, res, next) {
 
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   next();
-// });
-// const whitelist = ['http://localhost:3000', 'https://dina-cors-anywhere.herokuapp.com/https://tedashi-trained.herokuapp.com']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-
-// const allowlist = ['http://localhost:3000', 'https://tedashi-trained.herokuapp.com']
-// const corsOptionsDelegate = function (req, callback) {
-//   let corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
-console.log('Made it to API routes page!');
-//cors(corsOptions), 
 router.get('/', handleGetAll);
 router.get('/:id', handleGetOne);
 router.post('/', handleAdd);
